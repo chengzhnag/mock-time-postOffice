79fe680b949accd91ed92ba4bf06ba345ace3b43
chengzhnag <43592982+chengzhnag@users.noreply.github.com>
2020-09-06 20:00:18 +0800

    初始化创建定时任务

--- server/app.js
+++ server/app.js
@@ -62,6 +62,8 @@ app.use(session(sess))
 app.use('/', indexRouter);
 app.use('/send', sendRouter);
 
+require('./utils/initCronnJob'); // 初始化创建定时任务
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
---
+++ server/utils/initCronnJob.js
@@ -0,0 +1,49 @@
+var CronJob = require('cron').CronJob;
+var Record = require('../models/record.js');
+const {
+    sendEmail
+} = require('./sendEmail');
+
+(() => {
+    Record.find({ status: 1 }, (err, data) => {
+        if (err) return;
+        data.forEach(item => {
+            if (new Date(item['sendTime']) > new Date()) {
+                createCron(item);
+            }
+        })
+    })
+})();
+
+
+
+function createCron(body) {
+    let date = new Date(body.sendTime);
+    var job = new CronJob(date, () => {
+        // 发送
+        body.subject = `${body.name}通过时光邮局发送(http://email.zsjustn.top/)`;
+        sendEmail(body, suc => {
+            // 发送成功后更新记录状态
+            updateRecord(body);
+            console.log('保存发送成功的记录到数据库');
+        }, err => {
+            console.log('保存发送失败的记录到数据库');
+        });
+    }, null, false, 'America/Los_Angeles');
+    job.start();
+}
+
+
+function updateRecord(datas, success, fail) {
+    Record.findByIdAndUpdate(datas._id, {
+        status: 2
+    }, (err, ret) => {
+        if (err) {
+            console.log('更新失败')
+            fail && fail(err);
+        } else {
+            console.log('更新成功')
+            success && success(ret);
+        }
+    })
+}
